372a8ee1be0844c0c5bb05b3cd3f914d689f6692
ShmuelFine <shmuel.v.fine@gmail.com>
2025-07-24 09:45:49 +0300

    print util

--- KungFu Chess/Command.py
+++ KungFu Chess/Command.py
@@ -7,3 +7,9 @@ class Command:
     piece_id: str
     type: str               # "move" | "jump" | …
     params: List            # payload (e.g. ["e2", "e4"])
+
+    def __str__(self) -> str:
+        return f"Command(timestamp={self.timestamp}, piece_id={self.piece_id}, type={self.type}, params={self.params})"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
